fda9e6a7ee86155075de861d3299be5e1f22ffbd
Cuturov <oleg.cuturov@iis.utm.md>
2025-11-06 14:44:55 +0200

    Modificat ThreadOleg

--- LB_3/src/ThreadsOleg.java
+++ LB_3/src/ThreadsOleg.java
@@ -1,9 +1,10 @@
 public class ThreadsOleg {
 
+    // Thread 3 – Oleg, parcurgere de la început
     public static class Task3 implements Runnable {
         @Override
         public void run() {
-            System.out.println("Th3 started: Parcurgere array de la inceput");
+            System.out.println("Th3 (autor: Cuturov Oleg): De parcurs de la început intervalul [456, 1234]");
             System.out.println("Th3: Parcurgere DE LA ÎNCEPUT intervalul [456, 1234]");
 
             int[] array2 = LaboratorThreadsMain.ARRAY2;
@@ -19,16 +20,18 @@ public class ThreadsOleg {
 
             System.out.println(">>> Th3 FINAL: " + count + " elemente procesate");
 
+            // sincronizare comună
             LaboratorThreadsMain.SYNC.markFinished();
             LaboratorThreadsMain.SYNC.waitAll();
             LaboratorThreadsMain.SYNC.displayInOrder(3, LaboratorThreadsMain.DISCIPLINA);
         }
     }
 
+    // Thread 4 – Oleg, parcurgere de la sfârșit
     public static class Task4 implements Runnable {
         @Override
         public void run() {
-            System.out.println("Th4 started: Parcurgere array de la sfarsit");
+            System.out.println("Th4 (autor: Cuturov Oleg): De parcurs de la sfârșit intervalul [456, 1234]");
             System.out.println("Th4: Parcurgere DE LA SFÂRȘIT intervalul [456, 1234]");
 
             int[] array2 = LaboratorThreadsMain.ARRAY2;
@@ -44,6 +47,7 @@ public class ThreadsOleg {
 
             System.out.println(">>> Th4 FINAL: " + count + " elemente procesate");
 
+            // sincronizare comună
             LaboratorThreadsMain.SYNC.markFinished();
             LaboratorThreadsMain.SYNC.waitAll();
             LaboratorThreadsMain.SYNC.displayInOrder(4, LaboratorThreadsMain.GRUPA);
